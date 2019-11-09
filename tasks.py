f08751d53aff8d96b33e3ceb3baab433e610d32d
carlos-el <c-enriquez@outlook.com>
2019-11-09 15:14:02 +0100

    Adds CI and updates dependencies, closes #39.

--- tasks.py
+++ tasks.py
@@ -1,17 +1,21 @@
 from invoke import task
 
+# updates dependencies file
 @task 
 def createdependencies(c):
     c.run("pip3 freeze > requirements.txt")
 
+# updates dependencies in the system
 @task 
 def installdependencies(c):
     c.run("pip3 install -r requirements.txt")
 
+# runs tests and check coverage 
 @task
 def test(c):
     c.run("coverage run --source=events_microservice,notifications_microservice -m py.test")
 
+# create coverage report and uploads it to codecov
 @task
 def coverage(c):
     c.run("coverage xml")
