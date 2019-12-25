e05a9ca1bad52d126902dec3529cc8a9da8c012c
carlos-el <c-enriquez@outlook.com>
2019-12-25 19:12:59 +0100

    Change Dator used in apps, references #50.

--- events_microservice/app.py
+++ events_microservice/app.py
@@ -2,10 +2,10 @@ import falcon
 
 from events_microservice.resources.events_resource import EventsResource
 from events_microservice.resources.event_resource import EventResource
-from events_microservice.models.event_dator import EventDatorMock 
+from events_microservice.models.event_dator import MongoEventDator 
 
 
 api = application = falcon.API()
 
-api.add_route('/events', EventsResource(EventDatorMock()))
-api.add_route('/events/{id}', EventResource(EventDatorMock()))
+api.add_route('/events', EventsResource(MongoEventDator()))
+api.add_route('/events/{id}', EventResource(MongoEventDator()))
--- notifications_microservice/app.py
+++ notifications_microservice/app.py
@@ -2,10 +2,10 @@ import falcon
 
 from notifications_microservice.resources.notification_resource import NotificationResource
 from notifications_microservice.resources.notifications_resource import NotificationsResource
-from notifications_microservice.models.notification_dator import NotificationDatorMock
+from notifications_microservice.models.notification_dator import MongoNotificationDator
 
 
 api = application = falcon.API()
 
-api.add_route('/notifications', NotificationsResource(NotificationDatorMock()))
-api.add_route('/notifications/{id}', NotificationResource(NotificationDatorMock()))
+api.add_route('/notifications', NotificationsResource(MongoNotificationDator()))
+api.add_route('/notifications/{id}', NotificationResource(MongoNotificationDator()))
