7ce2538869fb9934536e943f5b034ec40d36b2e3
carlos-el <c-enriquez@outlook.com>
2019-12-25 19:05:08 +0100

    Adds dator for MongoDB, references #50.

--- events_microservice/models/event_dator.py
+++ events_microservice/models/event_dator.py
@@ -1,26 +1,89 @@
 # event_dator.py file, EventDator is currently a mock for giving data.
 
-from events_microservice.utils.singleton import Singleton
-from events_microservice.models.event import Event
+import pymongo
+from bson.objectid import ObjectId
+import json
 import datetime as dt
 
+from events_microservice.utils.singleton import Singleton
+from events_microservice.models.event import Event
+
+
+class MongoEventDator(metaclass=Singleton):
+    def __init__(self):
+        client = pymongo.MongoClient('127.0.0.1', 27017)
+        db = client.EventsMicroservice
+        self.__events = db.Events
+
+    def db_dict_to_event(self, data):
+        # Changes key name from '_id' to 'id' and cast key value from ObjectId to str
+        data["id"] = str(data.pop("_id"))
+        date = data['date'].split('-')
+        time = data['time'].split(':')
+        return Event(data["title"], data["description"], 
+                dt.date(int(date[0]), int(date[1]), int(date[2])), 
+                dt.time(int(time[0]), int(time[1]), int(time[2])), 
+                data["place"], data["organizer"], data["topics"], data["id"])
+
+    def get_all(self): 
+        evs = []
+        for data in self.__events.find():
+            evs.append(self.db_dict_to_event(data))
+
+        return evs
+
+    def get_by_id(self, id):
+        if not isinstance(id, str):
+            raise AttributeError("Parameter id must be of type str")
+
+        data = self.__events.find_one({"_id": ObjectId(id)})
+        if data is None:
+            raise ValueError("Could not find specified id.")
+        else:
+            return self.db_dict_to_event(data)
+
+    def save(self, event):
+        ev_json = event.to_json()
+        ev_json.pop("id")
+        data = self.__events.find_one_and_update({"_id": ObjectId(event.get_id())}, {'$set': ev_json}, 
+                                                return_document=pymongo.ReturnDocument.AFTER)
+
+        if data is None:
+            result = self.__events.insert_one(ev_json)
+            data = self.__events.find_one({"_id": ObjectId(result.inserted_id)})
+            return self.db_dict_to_event(data)
+        else:
+            return self.db_dict_to_event(data)
+
+
+    def delete_by_id(self, id):
+        if not isinstance(id, str):
+            raise AttributeError("Parameter id must be of type str")
+            
+        data = self.__events.find_one_and_delete({"_id": ObjectId(id)})
+        if data is None:
+            return None
+        else:
+            return self.db_dict_to_event(data)
+
+    
 
 class EventDatorMock(metaclass=Singleton):  
     def __init__(self):
         self.__data = {
-            0: Event("Evento 1, un titulo", "Breve (o no tanto) descripcion del evento 1. Relleno de contenido en este evento", dt.date(2020, 1, 1), dt.time(20, 50, 10), "Plaza Mayor 1", "Nadie real1.", "Tema libre1", 0),
-            1: Event("Evento 1, un titulo", "Breve (o no tanto) descripcion del evento 1. Relleno de contenido en este evento", dt.date(2020, 1, 1), dt.time(20, 50, 10), "Plaza Mayor 1", "Nadie real1.", "Tema libre1", 1),
-            2: Event("Evento 2, dos titulo", "Breve (o no tanto) descripcion del evento dos. Relleno de contenido en este evento", dt.date(2020, 2, 2), dt.time(20, 50, 20), "Plaza Mayor 2", "Nadie real2.", "Tema libre2", 2),
-            3: Event("Evento 3, tres titulo", "Breve (o no tanto) descripcion del evento 3. Relleno de contenido en este evento", dt.date(2020, 3, 3), dt.time(20, 50, 30), "Plaza Mayor 3", "Nadie real3.", "Tema libre3", 3),
-            4: Event("Evento 4,  cuatro titulo", "Breve (o no tanto) descripcion del evento cuatro. Relleno de contenido en este evento", dt.date(2020, 4, 4), dt.time(20, 50, 40), "Plaza Mayor 4", "Nadie real4.", "Tema libre4", 4),
+            "aaaaaaaaaaaaaaaaaaaaaaa0": Event("Evento 1, un titulo", "Breve (o no tanto) descripcion del evento 1. Relleno de contenido en este evento", dt.date(2020, 1, 1), dt.time(20, 50, 10), "Plaza Mayor 1", "Nadie real1.", "Tema libre1", "aaaaaaaaaaaaaaaaaaaaaaa0"),
+            "aaaaaaaaaaaaaaaaaaaaaaa1": Event("Evento 1, un titulo", "Breve (o no tanto) descripcion del evento 1. Relleno de contenido en este evento", dt.date(2020, 1, 1), dt.time(20, 50, 10), "Plaza Mayor 1", "Nadie real1.", "Tema libre1", "aaaaaaaaaaaaaaaaaaaaaaa1"),
+            "aaaaaaaaaaaaaaaaaaaaaaa2": Event("Evento 2, dos titulo", "Breve (o no tanto) descripcion del evento dos. Relleno de contenido en este evento", dt.date(2020, 2, 2), dt.time(20, 50, 20), "Plaza Mayor 2", "Nadie real2.", "Tema libre2", "aaaaaaaaaaaaaaaaaaaaaaa2"),
+            "aaaaaaaaaaaaaaaaaaaaaaa3": Event("Evento 3, tres titulo", "Breve (o no tanto) descripcion del evento 3. Relleno de contenido en este evento", dt.date(2020, 3, 3), dt.time(20, 50, 30), "Plaza Mayor 3", "Nadie real3.", "Tema libre3", "aaaaaaaaaaaaaaaaaaaaaaa3"),
+            "aaaaaaaaaaaaaaaaaaaaaaa4": Event("Evento 4,  cuatro titulo", "Breve (o no tanto) descripcion del evento cuatro. Relleno de contenido en este evento", dt.date(2020, 4, 4), dt.time(20, 50, 40), "Plaza Mayor 4", "Nadie real4.", "Tema libre4", "aaaaaaaaaaaaaaaaaaaaaaa4"),
         }
 
     def get_all(self): 
         return list(self.__data.values())
 
     def get_by_id(self, id): 
-        if not isinstance(id, int):
-            raise AttributeError("Parameter id must be of type integer")
+        if not isinstance(id, str):
+            raise AttributeError("Parameter id must be of type str")
         if id not in self.__data:
             raise ValueError("Could not find specified id.")
         else:
@@ -35,8 +98,11 @@ class EventDatorMock(metaclass=Singleton):
             return event
         else:
             # if it doesn't
-            new_id = max(self.__data.keys()) + 1
-
+            new_id = list(self.__data.keys())[len(self.__data)-1] 
+            last_number = str(int(new_id.split("a")[-1]) + 1)
+            new_id = new_id[:-len(last_number)]
+            new_id = new_id + last_number
+            
             new = Event(event.get_title(), event.get_description(), event.get_date(), event.get_time(
             ), event.get_place(), event.get_organizer(), event.get_topics(), new_id)
             self.__data[new_id] = new
@@ -44,7 +110,8 @@ class EventDatorMock(metaclass=Singleton):
             return new
 
     def delete_by_id(self, id):
-        if not isinstance(id, int):
-            raise AttributeError("Parameter id must be of type integer")
+        if not isinstance(id, str):
+            raise AttributeError("Parameter id must be of type str")
         if id in self.__data:
             return self.__data.pop(id, None)
+
--- notifications_microservice/models/notification_dator.py
+++ notifications_microservice/models/notification_dator.py
@@ -1,18 +1,81 @@
 # notification_dator.py file, NotificationDator is currently a mock for giving data.
 
+import pymongo
+from bson.objectid import ObjectId
+import json
+import datetime as dt
+
 from notifications_microservice.utils.singleton import Singleton
 from notifications_microservice.models.notification import Notification
-import datetime as dt
+
+
+class MongoNotificationDator(metaclass=Singleton):
+    def __init__(self):
+        client = pymongo.MongoClient()
+        db = client.NotificationsMicroservice
+        self.__notifications = db.Notifications
+
+    def db_dict_to_notification(self, data):
+        # Changes key name from '_id' to 'id' and cast key value from ObjectId to str
+        data["id"] = str(data.pop("_id"))
+        tmp = data["scheduled_time"].split('T')
+        tmp2 = tmp[1].split('.')
+        date = tmp[0].split('-')
+        time = tmp2[0].split(':')
+        return Notification(data['subject'], data['content'], data['to_mail'], 
+                            dt.datetime(int(date[0]), int(date[1]), int(date[2]), 
+                            int(time[0]), int(time[1]), int(time[2])), data["id"])
+
+    def get_all(self): 
+        nts = []
+        for data in self.__notifications.find():
+            nts.append(self.db_dict_to_notification(data))
+
+        return nts
+
+    def get_by_id(self, id):
+        if not isinstance(id, str):
+            raise AttributeError("Parameter id must be of type str")
+
+        data = self.__notifications.find_one({"_id": ObjectId(id)})
+        if data is None:
+            raise ValueError("Could not find specified id.")
+        else:
+            return self.db_dict_to_notification(data)
+
+    def save(self, event):
+        nt_json = event.to_json()
+        nt_json.pop("id")
+        data = self.__notifications.find_one_and_update({"_id": ObjectId(event.get_id())}, {'$set': nt_json}, 
+                                                return_document=pymongo.ReturnDocument.AFTER)
+
+        if data is None:
+            result = self.__notifications.insert_one(nt_json)
+            data = self.__notifications.find_one({"_id": ObjectId(result.inserted_id)})
+            return self.db_dict_to_notification(data)
+        else:
+            return self.db_dict_to_notification(data)
+
+
+    def delete_by_id(self, id):
+        if not isinstance(id, str):
+            raise AttributeError("Parameter id must be of type str")
+            
+        data = self.__notifications.find_one_and_delete({"_id": ObjectId(id)})
+        if data is None:
+            return None
+        else:
+            return self.db_dict_to_notification(data)
 
 
 class NotificationDatorMock(metaclass=Singleton): 
     def __init__(self):
         self.__data = {
-            0:  Notification("test subject 0", "test content 0"+'a'*30, "testmail0@testmail.com", dt.datetime.now()+dt.timedelta(1), 0),
-            1:  Notification("test subject 1", "test content 1"+'a'*30, "testmail1@testmail.com", dt.datetime.now()+dt.timedelta(1), 1),
-            2:  Notification("test subject 2", "test content 2"+'a'*30, "testmail2@testmail.com", dt.datetime.now()+dt.timedelta(1), 2),
-            3:  Notification("test subject 3", "test content 3"+'a'*30, "testmail3@testmail.com", dt.datetime.now()+dt.timedelta(1), 3),
-            4:  Notification("test subject 4", "test content 4"+'a'*30, "testmail4@testmail.com", dt.datetime.now()+dt.timedelta(1), 4),
+            "aaaaaaaaaaaaaaaaaaaaaaa0":  Notification("test subject 0", "test content 0"+'a'*30, "testmail0@testmail.com", dt.datetime.now()+dt.timedelta(1), "aaaaaaaaaaaaaaaaaaaaaaa0"),
+            "aaaaaaaaaaaaaaaaaaaaaaa1":  Notification("test subject 1", "test content 1"+'a'*30, "testmail1@testmail.com", dt.datetime.now()+dt.timedelta(1), "aaaaaaaaaaaaaaaaaaaaaaa1"),
+            "aaaaaaaaaaaaaaaaaaaaaaa2":  Notification("test subject 2", "test content 2"+'a'*30, "testmail2@testmail.com", dt.datetime.now()+dt.timedelta(1), "aaaaaaaaaaaaaaaaaaaaaaa2"),
+            "aaaaaaaaaaaaaaaaaaaaaaa3":  Notification("test subject 3", "test content 3"+'a'*30, "testmail3@testmail.com", dt.datetime.now()+dt.timedelta(1), "aaaaaaaaaaaaaaaaaaaaaaa3"),
+            "aaaaaaaaaaaaaaaaaaaaaaa4":  Notification("test subject 4", "test content 4"+'a'*30, "testmail4@testmail.com", dt.datetime.now()+dt.timedelta(1), "aaaaaaaaaaaaaaaaaaaaaaa4"),
         }
 
     def get_all(self):  
@@ -33,7 +96,10 @@ class NotificationDatorMock(metaclass=Singleton):
             return notification
         else:
             # if it doesn't
-            new_id = max(self.__data.keys()) + 1
+            new_id = list(self.__data.keys())[len(self.__data)-1] 
+            last_number = str(int(new_id.split("a")[-1]) + 1)
+            new_id = new_id[:-len(last_number)]
+            new_id = new_id + last_number
 
             new =  Notification(notification.get_subject(), notification.get_content(), notification.get_to_mail(), notification.get_scheduled_time(), new_id)
             self.__data[new.get_id()] = new
