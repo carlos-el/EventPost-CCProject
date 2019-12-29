ea71b186bd85f523ff3e272f4c01c2ddaaf4ad00
carlos-el <c-enriquez@outlook.com>
2019-12-29 13:00:00 +0100

    Adds cache settings, references #55

---
+++ notifications_microservice/settings.py
@@ -0,0 +1,12 @@
+from falcon_caching import Cache
+
+# Setup the cache instance
+cache = Cache(
+    config=
+    {
+        # Sets management strategy of the cache
+        'CACHE_EVICTION_STRATEGY': 'time-based',  
+        # Sets type of cache ('null' sets no cache, used for development and unit test)
+        # A production environment should use onother type of cache such as 'simple.'
+        'CACHE_TYPE': 'null' 
+    })
