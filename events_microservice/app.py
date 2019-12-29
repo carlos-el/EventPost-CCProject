bc3c61d95010714bc923ab6ac6209ca5241a024c
carlos-el <c-enriquez@outlook.com>
2019-12-29 13:02:30 +0100

    Adds cache middleware to app, closes #55

--- events_microservice/app.py
+++ events_microservice/app.py
@@ -1,11 +1,13 @@
 import falcon
+from events_microservice import settings
 
 from events_microservice.resources.events_resource import EventsResource
 from events_microservice.resources.event_resource import EventResource
 from events_microservice.models.event_dator import MongoEventDator 
 
+cache = settings.cache
 
-api = application = falcon.API()
+api = application = falcon.API(middleware=cache.middleware)
 
 api.add_route('/events', EventsResource(MongoEventDator()))
 api.add_route('/events/{id}', EventResource(MongoEventDator()))
--- notifications_microservice/app.py
+++ notifications_microservice/app.py
@@ -1,11 +1,13 @@
 import falcon
+from notifications_microservice import settings
 
 from notifications_microservice.resources.notification_resource import NotificationResource
 from notifications_microservice.resources.notifications_resource import NotificationsResource
 from notifications_microservice.models.notification_dator import MongoNotificationDator
 
+cache = settings.cache
 
-api = application = falcon.API()
+api = application = falcon.API(middleware=cache.middleware)
 
 api.add_route('/notifications', NotificationsResource(MongoNotificationDator()))
 api.add_route('/notifications/{id}', NotificationResource(MongoNotificationDator()))
